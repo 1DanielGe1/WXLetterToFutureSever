a6ce9641715b732d3a1ec9ba8bbee7b05dcf703d
GeAo <danielge0608@gmail.com>
2019-04-26 19:54:15 +0800

    update by geao

--- src/net/zlg/wxlettertofutureserver/GetIfoServlet.java
+++ src/net/zlg/wxlettertofutureserver/GetIfoServlet.java
@@ -12,11 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import net.sf.json.JSONObject;
 
